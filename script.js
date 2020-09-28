807ea358bb626d0fc7e68faa417cd365182638c2
alaa140101 <alaa140011@gmail.com>
2020-09-28 06:39:41 +0300

    Animations CSS

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const menuBars = document.getElementById('menu-bars');
 const overlay = document.getElementById('overlay');
-const navs = document.querySelectorAll('nav li');
+const navsNodeList = document.querySelectorAll('nav li');
+const navsArray = Array.prototype.slice.call(navsNodeList);
 
 function toggleNav() {
   // Toggle: Menu Bars Open/Closed
@@ -10,19 +11,28 @@ function toggleNav() {
     overlay.classList.remove('overlay-slide-left');
     // Animate In - Overlay
     overlay.classList.add('overlay-slide-right');
+    // Animate In - Overlay
+    navsArray.forEach((nav) => {
+      nav.classList.remove(`slide-out-${navsArray.indexOf(nav) + 1}`);
+      nav.classList.add(`slide-in-${navsArray.indexOf(nav) + 1}`);
+    });
   } else {
     overlay.classList.remove('overlay-slide-right');
     // Animate Out - Overlay
     overlay.classList.add('overlay-slide-left');
+    // Animate Out - Overlay
+    navsArray.forEach((nav) => {
+      nav.classList.remove(`slide-in-${navsArray.indexOf(nav) + 1}`);
+      nav.classList.add(`slide-out-${navsArray.indexOf(nav) + 1}`);
+    });
   }
 }
-
 
 // Event Listeners
 menuBars.addEventListener('click', toggleNav);
 // for (let index = 0; index < navList.length; index++) {
 //   navList[index].addEventListener('click', toggleNav);
 // }
-navs.forEach((nav) => {
+navsNodeList.forEach((nav) => {
   nav.addEventListener('click', toggleNav);
 });
